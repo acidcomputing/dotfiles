ea6f93bfbf122968d4f08f9736cabae11510e29f
acidcomputing <shoe.gazing421@slmails.com>
2025-09-17 11:40:42 -0600

    Create .aliases.zsh
    Created .aliases.zsh file.

---
+++ .aliases.zsh
@@ -0,0 +1,13 @@
+## A c i d  C o m p u t i n g  ##############
+# Zsh aliases
+
+# Listing
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Personal
+alias fetch='fastfetch'
+alias ff='fastfetch'
+alias goodbye='sudo shutdown now'
+alias zzz='systemctl sleep'
