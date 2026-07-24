7735a8c97737c0500a7e08659209c7a8050b0e5f
Gabe Moya <gabemoya.dandelion090@aleeas.com>
2026-07-24 03:10:13 -0600

    zsh: updated '.aliases.zsh'

--- .aliases.zsh
+++ .aliases.zsh
@@ -6,21 +6,19 @@ alias ll='ls -l'
 alias lsa='ls -la'
 alias la='ls -A'
 alias l='ls -CF'
-alias ..='cd ..'
-alias ...='cd ../..'
 
-# editors
+# editors & programming
 alias e='emacsclient -nw'
+alias nv='nvim'
+alias neovim='nvim'
+alias p3='python3'
+alias py='python3'
 
 # personal
 alias ff='fastfetch'
 alias fetch='fastfetch --config none'
 alias :q='exit'
-alias tlscl='tailscale'
 
 # power
-alias byebye='sudo shutdown now'
 alias zzz='systemctl sleep'
 
-# other
-alias py='python3'
